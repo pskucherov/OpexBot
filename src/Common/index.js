92892567fdee3277fc08db91d0302b1407de7f5d
pskucherov <pskucherov@yandex.ru>
2022-05-11 21:49:29 +0300

    первые шаги (сделки) робота

--- src/Common/index.js
+++ src/Common/index.js
@@ -1,68 +1,83 @@
-class Common {
-    constructor(adviser, backtest, options = {
-        takeProfit: 3,
-        stopLoss: 1,
-        useTrailingStop: true,
-    }) {
-        // Если робот работает в режиме советника,
-        // то он только обозначает предположения, но не делает сделки.
-        this.adviser = Boolean(adviser);
+try {
+    class Common {
+        constructor(accountId, adviser, backtest, callbacks = {
+            subscribes: {},
+            setOrder: () => {},
+        }, options = {
+            takeProfit: 3,
+            stopLoss: 1,
+            useTrailingStop: true,
+        }) {
+            this.accountId = accountId;
 
-        // На сколько процентов от текущей цены выставлять takeProfit.
-        this.takeProfit = options.takeProfit;
+            // Если робот работает в режиме советника,
+            // то он только обозначает предположения, но не делает сделки.
+            this.adviser = Boolean(adviser);
+            this.backtest = Boolean(backtest);
 
-        // На сколько процентов от текущей цены выставлять stopLoss.
-        this.stopLoss = options.stopLoss;
+            // Методы, с помощью которых робот может общаться с внешним миром.
+            this.cb = callbacks;
 
-        // Автоматически переставлять stopLoss и takeProfit при движении цены в нужную сторону.
-        this.useTrailingStop = options.useTrailingStop;
+            // На сколько процентов от текущей цены выставлять takeProfit.
+            this.takeProfit = options.takeProfit;
 
-        this.processing();
-    }
+            // На сколько процентов от текущей цены выставлять stopLoss.
+            this.stopLoss = options.stopLoss;
 
-    /**
+            // Автоматически переставлять stopLoss и takeProfit при движении цены в нужную сторону.
+            this.useTrailingStop = options.useTrailingStop;
+
+            this.timer = 250;
+
+            this.processing();
+        }
+
+        subscribes() {
+        }
+
+        /**
      * Модуль обработки данных.
      * По сути бесконечный цикл, который обрабатывает входящие данные.
      */
-    processing() {
-        if (!this.inProgress) {
+        processing() {
+            if (!this.inProgress) {
+                setImmediate(() => this.processing());
+
+                return;
+            }
+
+            if (this.decisionBuy()) {
+                this.buy();
+            } else if (this.decisionSell()) {
+                this.sell();
+            }
+
             setImmediate(() => this.processing());
-
-            return;
         }
 
-        if (this.decisionBuy()) {
-            this.buy();
-        } else if (this.decisionSell()) {
-            this.sell();
-        }
-
-        setImmediate(() => this.processing());
-    }
-
-    /**
+        /**
      * Модуль принятия решений о покупке.
      * @returns {Boolean}
      */
-    decisionBuy() {
-        return false;
-    }
+        decisionBuy() {
+            return false;
+        }
 
-    /**
+        /**
      * Модуль принятия решений о продаже.
      * @returns {Boolean}
      */
-    decisionSell() {
-        return false;
-    }
+        decisionSell() {
+            return false;
+        }
 
-    setControlsCallbacks() {
+        setControlsCallbacks() {
         // Метод покупки
         // Продажи
         // Выставления заявки
-    }
+        }
 
-    setCurrentState(lastPrice, candles, balance, tickerInfo, orderbook) {
+        setCurrentState(lastPrice, candles, balance, tickerInfo, orderbook) {
         // Текущая цена
         // История свечей
         // История стакана
@@ -71,48 +86,103 @@ class Common {
         // Баланс
         // Информация об инструменте
 
-        this.candles = candles;
-        this.orderbook = orderbook;
-        this.lastPrice = lastPrice;
-        this.balance = balance;
-        this.tickerInfo = tickerInfo;
-    }
+            candles && (this.candles = candles);
+            orderbook && (this.orderbook = orderbook);
+            lastPrice && (this.lastPrice = lastPrice);
+            balance && (this.balance = balance);
+            tickerInfo && (this.tickerInfo = tickerInfo);
+        }
 
-    getCurrentState() {
-        return [this.candles, this.orderbook];
-    }
+        setBacktestState(step, interval, figi) {
+            step && (this.step = step);
+            interval && (this.interval = interval);
+            figi && (this.figi = figi);
+        }
 
-    start() {
-        this.inProgress = true;
-    }
+        getBacktestState() {
+            return {
+                name: this.name,
+                step: this.step,
+                interval: this.interval,
+                figi: this.figi,
+            };
+        }
 
-    stop() {
-        this.inProgress = false;
-    }
+        getCurrentState() {
+            return [this.candles, this.orderbook];
+        }
 
-    buy() {
+        getPrice(quotation) {
+            if (quotation.nano) {
+                return quotation.units + quotation.nano / 1e9;
+            }
 
-    }
+            return quotation.units;
+        }
 
-    sell() {
+        start() {
+            this.inProgress = true;
+            this.subscribes();
+        }
 
-    }
+        stop() {
+            this.inProgress = false;
+        }
 
-    setActiveOrder() {
+        buy() {
 
-    }
+        }
 
-    setTakeProfit() {
+        sell() {
 
-    }
+        }
 
-    setStopLoss() {
+        setActiveOrder() {
 
-    }
+        }
 
-    clearUnfulfilledOrders() {
+        setTakeProfit() {
+
+        }
+
+        setStopLoss() {
+
+        }
+
+        clearUnfulfilledOrders() {
         // Удалить неисполненные заявки
-    }
-}
+        }
 
-module.exports.Common = Common;
+        /**
+     * @copypaste https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+     * @param {Number} length
+     * @returns
+     */
+        genOrderId(length = 15) {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+            let result = '';
+
+            for (let i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+            }
+
+            if (!this.orderIds) {
+                this.orderIds = {};
+            }
+
+            // Сохраняем сгенереный ключ, чтобы точно был уникальным + ведём статистику насколько генератор ок.
+            if (this.orderIds[result]) {
+                ++this.orderIds[result];
+                result = this.genOrderId(length);
+            } else {
+                this.orderIds[result] = 1;
+            }
+
+            return result;
+        }
+    }
+
+    module.exports.Common = Common;
+} catch (e) {}
--- src/RandomExample/index.js
+++ src/RandomExample/index.js
@@ -1,14 +1,181 @@
-const { Common } = require('../Common');
+try {
+    const { Common } = require('../Common');
 
-/**
+    /**
  * Торговый робот без логики со случайным срабатыванием.
  * Работает только на покупку, дальше ждёт исполнения заявки.
  * После исполнения заявки ждёт выхода по TP или SP.
  */
-class RandomExample extends Common {
-    decisionBuy() {
-        return Math.floor(Math.random() * 100) > 50;
-    }
-}
+    class RandomExample extends Common {
+        constructor(...args) {
+            super(...args);
+            this.name = 'RandomExample';
+        }
 
-module.exports.RandomExample = RandomExample;
+        subscribes() {
+            const { subscribes } = this.cb;
+
+            const timer = time => new Promise(resolve => setTimeout(resolve, time));
+
+            if (subscribes.lastPrice) {
+                setImmediate(async () => {
+                    const lp = subscribes.lastPrice[0]((async function* () {
+                        while (this.inProgress) {
+                            await timer(this.timer);
+                            yield subscribes.lastPrice[1]();
+                        }
+                    }).call(this));
+
+                    for await (const price of lp) {
+                        if (price.lastPrice) {
+                            this.lastPrice = price.lastPrice.price;
+                        }
+                    }
+                });
+            }
+        }
+
+        decisionBuy() {
+            return Math.floor(Math.random() * 100) > 50;
+        }
+
+        // {
+        //     orderId: '31271158513',
+        //     executionReportStatus: 1,
+        //     lotsRequested: 1,
+        //     lotsExecuted: 1,
+        //     initialOrderPrice: { currency: 'rub', units: 1234, nano: 900000000 },
+        //     executedOrderPrice: { currency: 'rub', units: 123, nano: 120000000 },
+        //     totalOrderAmount: { currency: 'rub', units: 1231, nano: 200000000 },
+        //     initialCommission: { currency: 'rub', units: 0, nano: 310000000 },
+        //     executedCommission: { currency: 'rub', units: 0, nano: 0 },
+        //     aciValue: { currency: 'rub', units: 0, nano: 0 },
+        //     figi: 'BBG004730N88',
+        //     direction: 1,
+        //     initialSecurityPrice: { currency: 'rub', units: 123, nano: 490000000 },
+        //     orderType: 2,
+        //     message: '',
+        //     initialOrderPricePt: undefined
+        //   }
+
+        // {
+        //     orderId: '31271365988',
+        //     executionReportStatus: 4,
+        //     lotsRequested: 1,
+        //     lotsExecuted: 0,
+        //     initialOrderPrice: { currency: 'rub', units: 1083, nano: 400000000 },
+        //     executedOrderPrice: { currency: 'rub', units: 0, nano: 0 },
+        //     totalOrderAmount: { currency: 'rub', units: 0, nano: 0 },
+        //     initialCommission: { currency: 'rub', units: 0, nano: 280000000 },
+        //     executedCommission: { currency: 'rub', units: 0, nano: 0 },
+        //     aciValue: { currency: 'rub', units: 0, nano: 0 },
+        //     figi: 'BBG004730N88',
+        //     direction: 1,
+        //     initialSecurityPrice: { currency: 'rub', units: 108, nano: 340000000 },
+        //     orderType: 1,
+        //     message: '',
+        //     initialOrderPricePt: undefined
+        //   }
+
+        // {
+        //     orders: [
+        //       {
+        //         orderId: '31271197970',
+        //         executionReportStatus: 4,
+        //         lotsRequested: 1,
+        //         lotsExecuted: 0,
+        //         initialOrderPrice: [Object],
+        //         executedOrderPrice: [Object],
+        //         totalOrderAmount: [Object],
+        //         averagePositionPrice: [Object],
+        //         initialCommission: [Object],
+        //         executedCommission: [Object],
+        //         figi: 'BBG004730N88',
+        //         direction: 2,
+        //         initialSecurityPrice: [Object],
+        //         stages: [],
+        //         serviceCommission: [Object],
+        //         currency: 'RUB',
+        //         orderType: 1,
+        //         orderDate: 2022-05-11T17:44:19.359Z
+        //       },
+        //       {
+        //         orderId: '31271326321',
+        //         executionReportStatus: 4,
+        //         lotsRequested: 1,
+        //         lotsExecuted: 0,
+        //         initialOrderPrice: [Object],
+        //         executedOrderPrice: [Object],
+        //         totalOrderAmount: [Object],
+        //         averagePositionPrice: [Object],
+        //         initialCommission: [Object],
+        //         executedCommission: [Object],
+        //         figi: 'BBG004730N88',
+        //         direction: 2,
+        //         initialSecurityPrice: [Object],
+        //         stages: [],
+        //         serviceCommission: [Object],
+        //         currency: 'RUB',
+        //         orderType: 1,
+        //         orderDate: 2022-05-11T17:51:01.327Z
+        //       },
+        //       {
+        //         orderId: '31271365988',
+        //         executionReportStatus: 4,
+        //         lotsRequested: 1,
+        //         lotsExecuted: 0,
+        //         initialOrderPrice: [Object],
+        //         executedOrderPrice: [Object],
+        //         totalOrderAmount: [Object],
+        //         averagePositionPrice: [Object],
+        //         initialCommission: [Object],
+        //         executedCommission: [Object],
+        //         figi: 'BBG004730N88',
+        //         direction: 1,
+        //         initialSecurityPrice: [Object],
+        //         stages: [],
+        //         serviceCommission: [Object],
+        //         currency: 'RUB',
+        //         orderType: 1,
+        //         orderDate: 2022-05-11T17:53:06.925Z
+        //       }
+        //     ]
+        //   }
+
+        /*
+    {"orders":[{"orderId":"31271197970","executionReportStatus":4,"lotsRequested":1,"lotsExecuted":0,"initialOrderPrice":{"currency":"rub","units":1250,"nano":0},"executedOrderPrice":{"currency":"rub","units":0,"nano":0},"totalOrderAmount":{"currency":"rub","units":1250,"nano":0},"averagePositionPrice":{"currency":"rub","units":0,"nano":0},"initialCommission":{"currency":"rub","units":0,"nano":320000000},"executedCommission":{"currency":"rub","units":0,"nano":0},"figi":"BBG004730N88","direction":2,"initialSecurityPrice":{"currency":"rub","units":125,"nano":0},"stages":[],"serviceCommission":{"currency":"rub","units":0,"nano":0},"currency":"RUB","orderType":1,"orderDate":"2022-05-11T17:44:19.359Z"},{"orderId":"31271326321","executionReportStatus":4,"lotsRequested":1,"lotsExecuted":0,"initialOrderPrice":{"currency":"rub","units":1240,"nano":0},"executedOrderPrice":{"currency":"rub","units":0,"nano":0},"totalOrderAmount":{"currency":"rub","units":1240,"nano":0},"averagePositionPrice":{"currency":"rub","units":0,"nano":0},"initialCommission":{"currency":"rub","units":0,"nano":310000000},"executedCommission":{"currency":"rub","units":0,"nano":0},"figi":"BBG004730N88","direction":2,"initialSecurityPrice":{"currency":"rub","units":124,"nano":0},"stages":[],"serviceCommission":{"currency":"rub","units":0,"nano":0},"currency":"RUB","orderType":1,"orderDate":"2022-05-11T17:51:01.327Z"},{"orderId":"31271365988","executionReportStatus":4,"lotsRequested":1,"lotsExecuted":0,"initialOrderPrice":{"currency":"rub","units":1083,"nano":400000000},"executedOrderPrice":{"currency":"rub","units":0,"nano":0},"totalOrderAmount":{"currency":"rub","units":1083,"nano":400000000},"averagePositionPrice":{"currency":"rub","units":0,"nano":0},"initialCommission":{"currency":"rub","units":0,"nano":280000000},"executedCommission":{"currency":"rub","units":0,"nano":0},"figi":"BBG004730N88","direction":1,"initialSecurityPrice":{"currency":"rub","units":108,"nano":340000000},"stages":[],"serviceCommission":{"currency":"rub","units":0,"nano":0},"currency":"RUB","orderType":1,"orderDate":"2022-05-11T17:53:06.925Z"}]}
+    */
+        async buy() {
+            if (!this.buyT && this.lastPrice) {
+                this.buyT = 1;
+                const orderId = this.genOrderId();
+
+                // console.log(orderId);
+
+                // const q = await this.cb.postOrder(
+                //     this.accountId,
+                //     this.figi,
+                //     1,
+                //     {
+                //         ...this.lastPrice,
+                //         units: this.lastPrice.units - 15,
+                //     },
+                //     1, // OrderDirection.ORDER_DIRECTION_BUY,
+                //     1, // OrderType.ORDER_TYPE_LIMIT,
+                //     orderId, //: 'abc-fsdfdsfsdf-2',
+                // );
+
+                // console.log(q);
+                // console.log(JSON.stringify(await this.cb.getOrders(this.accountId)));
+            }
+        }
+
+        stop() {
+            super.stop();
+
+            // console.log('stopt');
+        }
+    }
+
+    module.exports.RandomExample = RandomExample;
+} catch (e) {}
