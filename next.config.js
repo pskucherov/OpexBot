16987a91ba8b8bfffe4306351e2c297549c9faf8
pskucherov <pskucherov@yandex.ru>
2022-05-23 04:55:44 +0300

    0.1.7

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const fs = require('fs');
+
+// Когда opexviewer лежит рядом с opexbot
+const moduleDist = '../opexviewer/build';
+
+// Когда opexviewer лежит внутри node_modules
+const packageDist = 'node_modules/opexviewer/build';
+
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'node_modules/opexviewer/build',
+    distDir: fs.existsSync(moduleDist) ? moduleDist : packageDist,
 };
 
 module.exports = nextConfig;
