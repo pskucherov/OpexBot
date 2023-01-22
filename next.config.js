5515670712ae35a6a405e5aedf8a247c96ad95e0
pskucherov <pskucherov@yandex.ru>
2023-01-22 14:40:38 +0300

    portable version

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
-/** @type {import('next').NextConfig} */
-const fs = require('fs');
+// /** @type {import('next').NextConfig} */
+// const fs = require('fs');
+// const { join } = require('path');
+// const path = require('path');
 
-// Когда opexviewer лежит рядом с opexbot
-const moduleDist = '../opexviewer/build';
+// // Когда opexviewer лежит рядом с opexbot
+// const moduleDist = path.join('../', 'opexviewer');
 
-// Когда opexviewer лежит внутри node_modules
-const packageDist = 'node_modules/opexviewer/build';
+// // Когда opexviewer лежит внутри node_modules
+// const packageDist = path.join('./', 'node_modules', 'opexviewer');
 
-const nextConfig = {
-    reactStrictMode: true,
-    distDir: fs.existsSync(moduleDist) ? moduleDist : packageDist,
-};
+// const PROJECT_ROOT = fs.existsSync(moduleDist) ? moduleDist : packageDist;
 
-module.exports = nextConfig;
+// console.log(path.resolve(packageDist));
+
+// const nextConfig = {
+//     reactStrictMode: true,
+//     // distDir: path.join(PROJECT_ROOT, 'build'),
+//     // basePath: path.resolve(PROJECT_ROOT),
+//     serverRuntimeConfig: {
+//         PROJECT_ROOT: path.resolve(PROJECT_ROOT)
+//     }
+// };
+
+// module.exports = nextConfig;
--- opexbot.js
+++ opexbot.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const path = require('path');
+const kill = require('kill-port');
 const concurrently = require('concurrently');
 const { app, BrowserWindow } = require('electron');
 
+// Opexviewer лежит рядом с opexbot
+const moduleDist = path.join('../', 'OpexViewer');
+
+// Opexviewer лежит внутри node_modules
+const packageDist = path.join('./', 'node_modules', 'OpexViewer');
+const viewerDir = fs.existsSync(moduleDist) ? moduleDist : packageDist;
+
 (async () => {
-    const { result } = await concurrently([
-        { command: 'npx next start', name: 'opexviewer' },
+    await kill(3000, 'tcp');
+    await kill(8000, 'tcp');
+
+    concurrently([
+        { command: `npx next start ${viewerDir}`, name: 'opexviewer' },
         { command: 'node src/main', name: 'opexbot' },
     ], {
         prefix: 'name',
@@ -32,9 +44,20 @@ const { app, BrowserWindow } = require('electron');
         })
         .catch(() => null);
 
-    app.on('window-all-closed', () => {
+    app.on('window-all-closed', async () => {
+        await kill(3000, 'tcp');
+        await kill(8000, 'tcp');
+
         if (process.platform !== 'darwin') {
             app.quit();
+            process.exit(0);
         }
+    });
+
+    process.on('SIGINT', async () => {
+        await kill(3000, 'tcp');
+        await kill(8000, 'tcp');
+
+        process.exit(0);
     });
 })();
