60f08d53e0456e4bd93104206d5309302f6ae26f
pskucherov <pskucherov@yandex.ru>
2023-01-23 09:37:26 +0300

    tmp electro

--- opexbot.js
+++ opexbot.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const kill = require('kill-port');
 const concurrently = require('concurrently');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
+
 
 // Opexviewer лежит рядом с opexbot
 const moduleDist = path.join('../', 'OpexViewer');
@@ -28,31 +29,55 @@ const viewerDir = fs.existsSync(moduleDist) ? moduleDist : packageDist;
 
     const createWindow = () => {
         const win = new BrowserWindow({
-            width: 800,
-            height: 600,
+            title: "Opexflow.com",
+            width: 1024,
+            height: 768,
+            show: false,
+            icon: __dirname + '/icons/o.ico',
+            webPreferences: {
+                webviewTag: true,
+                nativeWindowOpen: true,
+                nodeIntegration: true,
+                enableRemoteModule: true,
+            }
         });
 
+        win.removeMenu();
+        win.setMenu(null);
+
         win.loadURL('http://localhost:3000/');
+        win.once('ready-to-show', () => {
+            win.show();
+        })
+        return win;
     };
 
     app.whenReady()
         .then(() => { // eslint-disable-line promise/always-return
-            createWindow();
+            const win = createWindow();
+    
             app.on('activate', () => {
-                if (BrowserWindow.getAllWindows().length === 0) createWindow();
+                if (BrowserWindow.getAllWindows().length === 0) {
+                    createWindow();
+                }
+            });
+
+            win.webContents.setWindowOpenHandler((details) => {
+                shell.openExternal(details.url);
+                return { action: 'deny' };
+            });
+
+            app.on('window-all-closed', async () => {
+                await kill(3000, 'tcp');
+                await kill(8000, 'tcp');
+
+                if (process.platform !== 'darwin') {
+                    app.quit();
+                    process.exit(0);
+                }
             });
         })
         .catch(() => null);
-
-    app.on('window-all-closed', async () => {
-        await kill(3000, 'tcp');
-        await kill(8000, 'tcp');
-
-        if (process.platform !== 'darwin') {
-            app.quit();
-            process.exit(0);
-        }
-    });
 
     process.on('SIGINT', async () => {
         await kill(3000, 'tcp');
