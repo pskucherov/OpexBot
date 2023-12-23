54180aa1a61205e81c8c4b8500d304302e261dcb
pskucherov <pskucherov@yandex.ru>
2023-12-23 11:50:07 +0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,8 +40,7 @@ module.exports = {
         'no-extra-semi': 'error',
         'no-func-assign': 'error',
         'no-sparse-arrays': 'error',
-
-        // 'no-undef': 'error',
+        'no-undef': 'error',
         'no-unexpected-multiline': 'error',
         'lines-around-comment': ['error', { beforeLineComment: true, allowObjectStart: true, allowBlockStart: true, beforeBlockComment: true }],
         'no-unreachable': 'error',
