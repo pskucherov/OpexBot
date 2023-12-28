7601fe3ce9298d5dfb4c0009a788f49576049970
pskucherov <pskucherov@yandex.ru>
2023-12-28 23:45:45 +0300

    common.ts

--- bots/sandboxer/accounts.ts
+++ bots/sandboxer/accounts.ts
@@ -1,4 +1,6 @@
 import { createSdk } from 'tinkoff-sdk-grpc-js';
+import { MoneyValue } from 'tinkoff-sdk-grpc-js/dist/generated/common';
+import { OpenSandboxAccountResponse } from 'tinkoff-sdk-grpc-js/dist/generated/sandbox';
 import { Account, GetAccountsResponse } from 'tinkoff-sdk-grpc-js/dist/generated/users';
 
 export class Accounts {
@@ -8,8 +10,15 @@ export class Accounts {
         this.sdk = sdk;
     }
 
-    async create() {
-        await this.sdk.sandbox.openSandboxAccount({});
+    async create(): Promise<OpenSandboxAccountResponse> {
+        return await this.sdk.sandbox.openSandboxAccount({});
+    }
+
+    async payIn(props: {
+        accountId: string,
+        amount: MoneyValue,
+    }) {
+        return await this.sdk.sandbox.sandboxPayIn(props);
     }
 
     async list(): Promise<GetAccountsResponse> {
--- bots/sandboxer/sandboxer.ts
+++ bots/sandboxer/sandboxer.ts
@@ -1,24 +1,140 @@
+import { DeepPartial, MoneyValue } from 'tinkoff-sdk-grpc-js/dist/generated/common';
 import { Accounts } from './accounts';
 import { createSdk } from 'tinkoff-sdk-grpc-js';
+import { TOKEN } from '../../config';
+import { MarketDataRequest, SubscriptionAction, SubscriptionInterval } from 'tinkoff-sdk-grpc-js/dist/generated/marketdata';
 
 // import TelegramBot from 'node-telegram-bot-api';
-
-const TINKOFFTOKEN = '';
 
 // const TGBOTTOKEN = '';
 // const TGUSERID = '';
 
-const sdk = createSdk(TINKOFFTOKEN, 'sandboxer');
+let keepCalling = true;
+
+// setTimeout(function() {
+//     keepCalling = false;
+// }, 50000);
+
+const timer = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+
+const sdk = createSdk(TOKEN, 'sandboxer');
+
+// Сумма пополнения демо счёта.
+const payInAmount: MoneyValue = {
+    units: 100000,
+    nano: 0,
+    currency: '',
+};
 
 // const tgbot = new TelegramBot(TGBOTTOKEN, { polling: true });
 
+const instrumentsForTrade = [
+    '53b67587-96eb-4b41-8e0c-d2e3c0bdd234', // АФК система
+    '1c69e020-f3b1-455c-affa-45f8b8049234', // AFLT
+    '46ae47ee-f409-4776-bf20-43a040b9e7fb', // РусАгро
+    '30817fea-20e6-4fee-ab1f-d20fc1a1bb72', // АЛРОСА
+    'ebfda284-4291-4337-9dfb-f55610d0a907', // МКБ
+    'fa6aae10-b8d5-48c8-bbfd-d320d925d096', // Северсталь
+    'e2bd2eba-75de-4127-b39c-2f2dbe3866c3', // Эн+
+    '88e130e8-5b68-4b05-b9ae-baf32f5a3f21', // ФСК Россети
+    'aa183ebe-3dae-4f4b-b7a2-c03539375417', // Пятерочка
+    '962e2a95-02a9-4171-abd7-aa198dbe643a', // Газпром
+    '38be8280-96ef-45e9-b0ed-da76bc77fe7c', // Globaltrans Investment PLC
+    '509edd0c-129c-4ee2-934d-7f6246126da1', // Норильский никель
+    '62560f05-3fd0-4d65-88f0-a27f249cc6de', // РусГидро
+    '2dfbc1fd-b92a-436e-b011-928c79e805f2', // Интер РАО ЕЭС
+    '02cfdf61-6298-4c0f-a9ca-9cabc82afaf3', // ЛУКОЙЛ
+    '7132b1c9-ee26-4464-b5b5-1046264b61d9', // Магнитогорский металлургический комбинат
+    'ca845f68-6c43-44bc-b584-330d2a1e5eb7', // Магнит
+    '5e1c2634-afc4-4e50-ad6d-f78fc14a539a', // Московская Биржа
+    'cd8063ad-73ad-4b31-bd0d-93138d9e99a2', // МТС
+    '161eb0d0-aaac-4451-b374-f5d0eeb1b508', // НЛМК
+    '0da66728-6c30-44c4-9264-df8fac2467ee', // НОВАТЭК
+    '35fb8d6b-ed5f-45ca-b383-c4e3752c9a8a', // Ozon Holdings PLC
+    '9978b56f-782a-4a80-a4b1-a48cbecfd194', // ФосАгро
+    '03d5e771-fc10-438e-8892-85a40733612d', // ПИК
+    '10620843-28ce-44e8-80c2-f26ceb1bd3e1', // Полюс
+    '127361c2-32ec-448c-b3ec-602166f537ea', // Polymetal
+    'de08affe-4fbd-454e-9fd1-46a81b23f870', // Positive Technologies
+    '120a928b-b2d6-45d7-a445-f6e49614ae6d', // QIWI
+    'fd417230-19cf-4e7b-9623-f7c9ca18ec6b', // Роснефть
+    '02eda274-10c4-4815-8e02-a8ee7eaf485b', // Ростелеком
+    'f866872b-8f68-4b6e-930f-749fe9aa79c0', // РУСАЛ
+    'e6123145-9665-43e0-8413-cd61b8aa9b13', // Сбер Банк
+    '0d28c01b-f841-4e89-9c92-0ee23d12883a', // Селигдар
+    '7bedd86b-478d-4742-a28c-29d27f8dbc7d', // Сегежа
+    'a797f14a-8513-4b84-b15e-a3b98dc4cc00', // Сургутнефтегаз - привилегированные акции
+    'efdb54d3-2f92-44da-b7a3-8849e96039f6', // Татнефть - привилегированные акции
+    '6afa6f80-03a7-4d83-9cf0-c19d7d021f76', // TCS Group
+    '664921c5-b552-47a6-9ced-8735a3c6ca8a', // Юнипро
+    'b71bd174-c72c-41b0-a66f-5f9073e0d1f5', // ВК
+    '8e2b0325-0292-4654-8a18-4f63ed3b0e09', // Банк ВТБ
+    '10e17a87-3bce-4a1f-9dfc-720396f98a3c', // Yandex
+];
+
+interface IAccountsIds {
+    [key: string]: {
+        accountId: string,
+        amount: MoneyValue,
+    };
+}
+
 (async () => {
     const account = new Accounts(sdk);
+    await account.closeAll()
 
-    console.log(await account.closeAll()); // eslint-disable-line no-console
-    console.log(await account.list()); // eslint-disable-line no-console
+    // Создаём для каждого инструмента отдельный счёт в песочнице и пополняем на payInAmount.
+    // const accountsIds =
+    await instrumentsForTrade.reduce(async (acc: Promise<IAccountsIds>, val) => {
+        const { accountId } = await account.create();
 
-    console.log(await sdk.marketData.getLastPrices({ // eslint-disable-line no-console
-        instrumentId: ['e6123145-9665-43e0-8413-cd61b8aa9b13', '0da66728-6c30-44c4-9264-df8fac2467ee'],
-    }));
+        await account.payIn({
+            accountId,
+            amount: payInAmount,
+        });
+
+        const data = await acc;
+
+        return {
+            ...data,
+            [val]: {
+                accountId,
+                amount: payInAmount,
+            },
+        };
+    }, Promise.resolve(<IAccountsIds>{}));
+
+    // console.log('accountsIds', accountsIds);
+    // console.log(await account.list());
+
+
+    //генератор подписки на свечи
+    async function* createSubscriptionCandleRequest(): AsyncIterable<DeepPartial<MarketDataRequest>> {
+        while (keepCalling) {
+            await timer(1000);
+            yield MarketDataRequest.fromPartial({
+                subscribeCandlesRequest: {
+                    subscriptionAction: SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                    instruments: instrumentsForTrade.map(i => {
+                        return {
+                            instrumentId: i,
+                            interval: SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+                        };
+                    }),
+                    waitingClose: true,
+                },
+            });
+        }
+    }
+
+    const response = sdk.marketDataStream.marketDataStream(createSubscriptionCandleRequest());
+
+    for await (const num of response) {
+        const { candle } = num || {};
+
+        if (candle) {
+            console.log(JSON.stringify(num, null, 4)); // eslint-disable-line no-console
+        }
+    }
+
 })();
